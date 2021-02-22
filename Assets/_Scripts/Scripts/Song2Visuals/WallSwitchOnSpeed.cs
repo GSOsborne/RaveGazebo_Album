53d15e0f80a56d178cb00acc243b72609d615bc7
GSOsborne <gs.osborne.pro@gmail.com>
2021-02-22 18:01:40 -0500

    Finished off creating the square floater prefabs, wrote a quick script to change between the wall types on speed change, not set on if that's the last of the changes on speed.

---
+++ Assets/_Scripts/Scripts/Song2Visuals/WallSwitchOnSpeed.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static DavisDnB_AudioManager;
+
+public class WallSwitchOnSpeed : MonoBehaviour
+{
+    public GameObject slowWalls, medWalls, fastWalls;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DavisDnB_AudioManager.PlaybackSpeedChange += ChangeWalls;
+        ChangeWalls(PlaybackSpeed.Medium);
+        
+    }
+
+    void ChangeWalls(PlaybackSpeed speed)
+    {
+        switch (speed)
+        {
+            case PlaybackSpeed.Slow:
+                medWalls.SetActive(false);
+                fastWalls.SetActive(false);
+                slowWalls.SetActive(true);
+                break;
+            case PlaybackSpeed.Medium:
+                slowWalls.SetActive(false);
+                fastWalls.SetActive(false);
+                medWalls.SetActive(true);
+                break;
+            case PlaybackSpeed.Fast:
+                slowWalls.SetActive(false);
+                medWalls.SetActive(false);
+                fastWalls.SetActive(true);
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
